1e588be0f094dfbd178af3e0d5a8c8ddf535b279
SAZO54 <zosaro2126@gmail.com>
2024-05-06 21:25:27 +0900

    Create UI for ship status screen

--- src/components/ShipStatus.vue
+++ src/components/ShipStatus.vue
@@ -1,9 +1,106 @@
 <script setup>
+import { ref } from 'vue';
+import TransactionTable from './Base/TransactionTable.vue';
+
+// const transactions = ref([
+//   { id: '1', name: 'Item A', status: 'Packing', destination: 'USA', platform: 'Mercari' },
+//   // add more items
+// ]);
+// const fields = ['id', 'name', 'status', 'destination', 'platform'];
+
+/**
+ * map button
+ */
+const worldActive = ref(true);
+
+function mapTabSwitch() {
+  worldActive.value = !worldActive.value;
+}
+
+console.log("worldActive", worldActive.value);
+
 </script>
 
 <template>
-  <p>This is 配送状況 page.</p>
+  <div class="flex">
+    <div>
+      <div class="ship-container" v-if="worldActive">
+        <div class="flex">
+          <button class="btn btn-pink" @click="mapTabSwitch">world</button>
+          <button class="btn disabled-btn jp-btn" @click="mapTabSwitch">japan</button>
+        </div> 
+        <div class="map-container" v-if="worldActive">
+          <img src="@/assets/world.png" alt="World Image" class="world-img"/>
+        </div>
+      </div>
+      <div class="ship-container" v-else>
+        <div class="flex">
+          <button class="btn disabled-btn" @click="mapTabSwitch">world</button>
+          <button class="btn btn-pink jp-btn" @click="mapTabSwitch">japan</button>
+        </div> 
+        <div class="map-container">
+          <img src="@/assets/japan.png" alt="Japan Image" class="japan-img"/>
+        </div>
+      </div>
+    </div>
+    <TransactionTable />
+  </div>
 </template>
 
 <style scoped>
+.ship-container {
+  margin: 50px 40px;
+}
+
+.flex {
+  display: flex;
+}
+
+.btn {
+  width: 80px;
+  height: 40px;
+  border-radius: 3px;
+  font-size: 19px;
+}
+
+.btn-pink {
+  background-color: #ECB1C4;
+  border: #ECB1C4;
+  color: #FAFCFD;
+}
+
+.btn-pink:hover {
+  background-color: #EDA3B8;
+  border: #EDA3B8;
+  color: #FAFCFD;
+}
+
+.disabled-btn, .disabled-btn:hover {
+  background-color: #e1d1d6;
+  border: #e1d1d6;
+  color: #8f8f8f;
+}
+
+.jp-btn {
+  margin-left: 5px;
+}
+
+.map-container {
+  background-color: #fff1fc7a;
+  width: 900px;
+  height: 679px;
+  margin-top: 20px;
+}
+
+.world-img {
+  width: 100%;
+  height: 90%;
+  object-fit: cover;
+}
+
+.japan-img {
+  width: 70%;
+  height: 100%;
+  object-fit: cover;
+}
 </style>
